33154520e54fa317324ac9d0f8a8255f6e2e3681
sidbola <sid.s.bola@gmail.com>
2019-06-24 11:01:07 -0700

    Removed wildcard import

--- lib/src/main/java/com/grio/lib/features/editor/views/ScreenAnnotator.kt
+++ lib/src/main/java/com/grio/lib/features/editor/views/ScreenAnnotator.kt
@@ -1,7 +1,11 @@
 package com.grio.lib.features.editor.views
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
