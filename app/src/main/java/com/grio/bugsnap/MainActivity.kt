e22b064af7c93a88167a264f6af3b22c75066068
sidbola <sid.s.bola@gmail.com>
2019-07-02 11:24:57 -0700

    Removes button used for debugging

--- app/src/main/java/com/grio/bugsnap/MainActivity.kt
+++ app/src/main/java/com/grio/bugsnap/MainActivity.kt
@@ -1,29 +1,12 @@
 package com.grio.bugsnap
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import com.grio.lib.core.extension.screenshot
-import com.grio.lib.features.BugSnap
-import com.grio.lib.features.editor.AnnotationActivity
-import com.grio.lib.features.editor.DataHolder
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        debugButton.setOnClickListener {
-            val rootView = window?.decorView as View
-            val bitmap = rootView.screenshot()
-
-            val intent = Intent(this, AnnotationActivity::class.java)
-            DataHolder.data = bitmap
-            startActivity(intent)
-        }
     }
 }
