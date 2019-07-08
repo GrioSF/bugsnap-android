b387db786ce6d63222a132336a44bcf73d12050a
Caleb Chiesa <email@orbyt.me>
2019-07-08 10:04:51 -0700

    Adds some content to sample app. Adds check mark. (#25)

--- app/src/main/java/com/grio/bugsnap/MainActivity.kt
+++ app/src/main/java/com/grio/bugsnap/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.grio.bugsnap
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var animals = mutableListOf<Drawable>(getDrawable(R.drawable.dog), getDrawable(R.drawable.dog_bike), getDrawable(R.drawable.llama), getDrawable(R.drawable.frog), getDrawable(R.drawable.koala))
+        var adapter = AnimalsAdapter(animals)
+        recycler_view.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        recycler_view.adapter = adapter
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
