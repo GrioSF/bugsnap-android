9550d89c1094859a7a977dc831ba915554e79b49
Jeffrey Rogiers <jeffrey.rogiers@gmail.com>
2019-08-01 15:52:49 -0700

    Cleaned up unused imports

--- lib/src/main/java/com/grio/lib/features/recorder/ReporterActivity.kt
+++ lib/src/main/java/com/grio/lib/features/recorder/ReporterActivity.kt
@@ -2,25 +2,13 @@ package com.grio.lib.features.recorder
 
 import android.os.Bundle
 import android.view.*
-import android.view.animation.DecelerateInterpolator
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.children
-import androidx.transition.ChangeBounds
-import androidx.transition.TransitionManager
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.gson.Gson
 import com.grio.lib.R
 import com.grio.lib.core.di.DaggerInjector
 import com.grio.lib.core.extension.observe
 import com.grio.lib.core.extension.viewModels
 import com.grio.lib.core.platform.BaseActivity
-import com.grio.lib.features.editor.*
-import com.grio.lib.features.editor.views.ScreenAnnotator
-import com.grio.lib.features.editor.views.Tool
-import com.grio.lib.features.editor.views.ToolOptions
-import kotlinx.android.synthetic.main.a_annotation.*
 import com.grio.lib.features.LogSnapshotManager
 import kotlinx.android.synthetic.main.a_reporter.*
 
